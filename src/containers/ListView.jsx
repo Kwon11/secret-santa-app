5948e52a02017597467d9e6ac3618795c88e9b41
Chan <chanthemancan@gmail.com>
2017-05-31 21:10:13 -0700

    ListView now renders items based on groups parameter from url

--- src/containers/ListView.jsx
+++ src/containers/ListView.jsx
@@ -4,12 +4,36 @@ import { connect } from 'react-redux';
 class ListView extends Component {
   constructor(props) {
     super(props);
+    if (this.props.groups) {
+      this.state = {
+        list: this.props.groups[this.props.location.search.slice(7)].targetWish.items
+      }
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.groups) {
+      this.setState({
+        list: nextProps.groups[nextProps.location.search.slice(7)].targetWish.items
+      })
+    }
+  }
+
+  renderList () {
+    return this.state.list.map((item, index) => {
+      return (
+          <li key={index}>{item}</li>
+        )
+    });
   }
 
   render () {
     return (
         <div>
           <h3>LIST VIEEEWWW </h3>
+          <ul>
+            {this.renderList()}
+          </ul>
         </div>
       )
   }
