04b867a253af465ea90654b860f3d7444396b388
Chan <chanthemancan@gmail.com>
2017-06-01 17:37:12 -0700

    EditList is controlled component, with value of searchbar set to state

--- src/containers/EditList.jsx
+++ src/containers/EditList.jsx
@@ -5,13 +5,23 @@ import { Link } from 'react-router-dom';
 class EditList extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchTerm: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange (event) {
+    event.preventDefault();
+    this.setState({searchTerm: event.target.value})
+    console.log(this.state.searchTerm);
   }
 
   render () {
     return (
         <div>
           <ul>
-          <li><form><label>Search For Amazon Items</label><input></input></form></li>
+          <li><form><label>Search For Amazon Items</label><input value={this.state.searchTerm} className="searchBar" onChange={this.handleChange} placeholder="Search for an Item!"></input></form></li>
           <li>this.renderList method for when this has search</li>
           <li> or put that in a whole new component </li>
           </ul>
