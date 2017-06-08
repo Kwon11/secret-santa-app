88dd7722b4be62539434a5c408f7783db214ff15
Chan Kwon <chanthemancan@gmail.com>
2017-06-07 18:14:01 -0700

    connected to local MySQL

--- server/server.js
+++ server/server.js
@@ -6,6 +6,29 @@ var aws = require('aws-lib');
 
 var prodAdv = aws.createProdAdvClient(process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY, process.env.AWS_ASSOCIATE_TAG)
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'secretSanta69',
+  database: 'secretSanta'
+});
+/*
+connection.connect((error) => {
+  if (error) {
+    console.log('error connecting to MySQL', error);
+  }
+  connection.query('CREATE TABLE test(id int primary key auto_increment, name varchar(255), age int, address text)', function(err, result) {
+    if (err) throw err
+    connection.query('INSERT INTO test (name, age, address) VALUES (?, ?, ?)', ['Larry', '41', 'California, USA'], function(err, result) {
+      if (err) throw err
+      connection.query('SELECT * FROM test', function(err, results) {
+        if (err) throw err
+      })
+    })
+  }) 
+})
+*/
 
 
 app.use(express.static(path.join(__dirname, '../dist')));
