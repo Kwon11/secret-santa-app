01a6f897810ae93542ae6af2ad58fe916797790c
Chan Kwon <chanthemancan@gmail.com>
2017-06-12 12:12:41 -0700

    Delete InvitationsList.jsx and Invitation. Handle invites from group-detail

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -7,7 +7,6 @@ import EventDetail from '../containers/EventDetail.jsx';
 import Settings from '../containers/Settings.jsx';
 import Chat from '../containers/Chat.jsx';
 import EditList from '../containers/EditList.jsx';
-import InvitationsList from '../containers/InvitationsList.jsx';
 
 export default class MainView extends Component {
   constructor(props) {
@@ -25,7 +24,6 @@ export default class MainView extends Component {
             <Route path="/Chat" component={Chat}/>
             <Route path="/Settings" component={Settings}/>
             <Route path="/EditList" component={EditList} />
-            <Route path="/Invitations" component={InvitationsList} />
           </div>
         </div>
       );
